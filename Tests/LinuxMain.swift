6ff672bccecf0b53c65b5d6d15d1acc1b4aa0b33
Thi <t@thi.im>
2019-04-13 11:57:36 +0900

    Generate LinuxMain and XCTestManifests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,25 +1,8 @@
 import XCTest
-@testable import GuakaTests
 
+import GuakaTests
 
-XCTMain([
-    testCase(ArgTokenTypeTests.allTests),
-    testCase(CommandExecutionTests.allTests),
-    testCase(CommandHelpTests.allTests),
-    testCase(CommandParsingTests.allTests),
-    testCase(CommandTests.allTests),
-    testCase(CommandRunTests.allTests),
-    testCase(CommandTest.allTests),
-    testCase(CustomFlagTypesTests.allTests),
-    testCase(ErrorTests.allTests),
-    testCase(FlagHelpTests.allTests),
-    testCase(FlagSetTests.allTests),
-    testCase(FlagsTests.allTests),
-    testCase(FlagTests.allTests),
-    testCase(HelpGeneratorSubclassingTests.allTests),
-    testCase(HelpGeneratorTests.allTests),
-    testCase(HelpTests.allTests),
-    testCase(LevenshteinTests.allTests),
-    testCase(ParsingTests.allTests),
-    testCase(ValidationTests.allTests),
-])
+var tests = [XCTestCaseEntry]()
+tests += GuakaTests.__allTests()
+
+XCTMain(tests)
