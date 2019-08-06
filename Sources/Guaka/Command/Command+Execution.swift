41bf9aeed227bdb8042c3cedfc67ff7d6385e94f
Makeeyaf <makeeyaf@gmail.com>
2019-08-06 22:26:34 +0900

    Remove whitespace-only lines.

--- Sources/Guaka/Command/Command+Execution.swift
+++ Sources/Guaka/Command/Command+Execution.swift
@@ -32,7 +32,7 @@ extension Command {
     handleResult(res)
   }
 
-  
+
   /// Get the actual command to execute based on the current command and the arguments passed
   ///
   /// - parameter commandLineArgs: the command line arguments passed
@@ -50,22 +50,22 @@ extension Command {
   public func commandToExecute(commandLineArgs: [String]) -> Command {
     return actualCommand(forCommand: self, arguments: Array(commandLineArgs.dropFirst())).0
   }
-  
+
   /// Validate a Command it flags and subcommands
   ///
   /// - Throws: error if command, subcommands or flags are not valid
   public func validate() throws {
     _ = try self.name()
-    
+
     for flag in self.flags {
       try flag.validate()
     }
-    
+
     for command in self.commands {
       try command.validate()
     }
   }
-  
+
   /// Validate a Command it flags and subcommands
   ///
   /// - Throws: error if command, subcommands or flags are not valid
@@ -80,59 +80,59 @@ extension Command {
       fail(statusCode: -2, errorMessage: "Unknown error ocurred")
     }
   }
-  
-  
+
+
   /// Execute a command with flags and positional arguments
   func execute(flags: Flags, arguments: [String]) {
     printDeprecationMessages(flags: Array(flags.flags))
-    
+
     // FIXME: Refactor and simplify
     if
       let inheritablePreRun = self.findAdequateInheritableRun(forPre: true),
       inheritablePreRun(flags, arguments) == false
     { return }
-    
+
     if
       let preRun = preRun,
       preRun(flags, arguments) == false
     { return }
-    
+
     self.run?(flags, arguments)
-    
+
     if
       let postRun = postRun,
       postRun(flags, arguments) == false
     { return }
-    
+
     _ = self.findAdequateInheritableRun(forPre: false)?(flags, arguments)
   }
-  
+
 }
 
 
 extension Command {
-  
+
   /// Find the preRun or Post run for the current command
   /// This searches for the parent post/pre run blocks
   func findAdequateInheritableRun(forPre: Bool) -> ConditionalRun?  {
-    
+
     var cmd: Command? = self
-    
+
     while true {
       let toFind = forPre ? cmd?.inheritablePreRun : cmd?.inheritablePostRun
-      
+
       if let toFind = toFind { return toFind }
-      
+
       cmd = cmd?.parent
       if cmd == nil { return nil }
     }
   }
-  
+
 }
 
 
 extension Command {
-  
+
   /// Handles the result of an execution
   fileprivate func handleResult(_ result: Result<String?, Error>) {
     switch result {
@@ -145,7 +145,7 @@ extension Command {
       guard case let CommandError.commandGeneralError(command, error) = error else {
         return
       }
-      
+
       let helpGenerator = GuakaConfig.helpGenerator.init(command: command)
       if let error = error as? CommandError {
         printToConsole(helpGenerator.errorString(forError: error))
@@ -155,21 +155,21 @@ extension Command {
       break
     }
   }
-  
-  
+
+
   /// Prints the deprecation message
   fileprivate func printDeprecationMessages(flags: [Flag]) {
-    
+
     let helpGenerator = GuakaConfig.helpGenerator.init(command: self)
-    
+
     if let deprecationMessage = helpGenerator.deprecationSection {
       printToConsole(deprecationMessage)
     }
-    
+
     flags.map { FlagHelp(flag: $0) }
       .filter { $0.isDeprecated && $0.wasChanged }
       .compactMap { helpGenerator.deprecationMessage(forDeprecatedFlag: $0) }
       .forEach { printToConsole($0) }
   }
-  
+
 }
